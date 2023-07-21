97e7c37a5f2ef72a7cf286ec7a3bd2a0ea05cc61
Putinka1993 <lipkin1993vladislav@gmail.com>
2023-07-21 10:10:29 +0300

    task6 ready

---
+++ task6/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 != 0)
+{
+    Console.WriteLine("NO!!");  
+}
+else Console.WriteLine("ohh YESS");
+
+
+
