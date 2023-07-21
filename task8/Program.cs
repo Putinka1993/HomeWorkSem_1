e16ee937aa43dd5f8ab94d0141ef8622d01bf009
Putinka1993 <lipkin1993vladislav@gmail.com>
2023-07-21 11:33:23 +0300

    task8 ready

---
+++ task8/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int c = 1; //count
+
+while (c != n+1)
+{
+    if (n <= 1)
+    {
+        Console.WriteLine("Некорректное число");
+        c+=n;
+    }
+    else if (c % 2 == 0) 
+    {
+        Console.WriteLine(c);
+        c++;
+    }
+   else c++;
+
+}
