aa22482cce3fe4ba1c88ccf82d201397c284b848
Putinka1993 <lipkin1993vladislav@gmail.com>
2023-07-21 09:25:52 +0300

    task4 ready

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+List<int> numbers = new List<int>();
+
+int n = 3, i = 0;
+
+while (i != n)
+{
+    Console.Write("Введите число: ");
+    int num = int.Parse(Console.ReadLine());
+    numbers.Add(num);
+    i++;
+}
+
+Console.WriteLine("Maximum " + numbers.Max());
+
